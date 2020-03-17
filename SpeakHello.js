b2c5c73319c4296987bae54f56257907ce094e9d
tatianadiam <61379633+tatianadiam@users.noreply.github.com>
2020-03-17 22:20:11 +0200

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,20 @@
+
+// (function(window){
+// 	var helloSpeaker = new Object();
+// 	var speakWord = "Hello";
+// 	helloSpeaker.speak=function (name) {
+// 	  console.log(speakWord + " " + name)
+// 	};
+// 	window.helloSpeaker=helloSpeaker;
+// })(window);
+
+
+
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
